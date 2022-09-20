7b558f09f2ac5b3660246fa2d6fc4a3d2da54f9e
matheusgondra <matheusgondra951@gmail.com>
2022-09-19 21:19:14 -0300

    Adicionando um ThemeProvider e um tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
 
 const GlogalStyle = createGlobalStyle`
 	* {
@@ -8,15 +8,34 @@ const GlogalStyle = createGlobalStyle`
 	}
 `;
 
+const theme = {
+	colors: {
+		blue: {
+			100: "#96B9FD",
+			200: "#7BA4FC",
+			300: "#5081FB",
+			800: "#051D3B"
+		}
+	},
+	fonts: {
+		sm: "12px",
+		md: "14px",
+		lg: "16px",
+		xl: "21px"
+	}
+}
+
 const AppContainer = styled.div`
 
 `;
 
 function App() {
 	return (
-		<AppContainer>
-			<GlogalStyle />
-		</AppContainer>
+		<ThemeProvider theme={theme}>
+			<AppContainer>
+				<GlogalStyle />
+			</AppContainer>
+		</ThemeProvider>
 	);
 }
 

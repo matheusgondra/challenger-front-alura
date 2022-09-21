b324ba7bc921349dc3b1cbe3b0d49e947d3837fd
matheusgondra <matheusgondra951@gmail.com>
2022-09-21 13:32:03 -0300

    Criando menu lateral

--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -8,11 +8,25 @@ export function Home() {
             <Header />
             <main>
                 <MenuNav />
-                <section id="projects">
-                    <h2>Seu projeto</h2>
-                    <input placeholder="Nome do seu projeto" />
-                    <textarea placeholder="Descrição do seu projeto" />
-                </section>
+                <form>
+                    <section id="projects">
+                        <h2>Seu projeto</h2>
+                        <input placeholder="Nome do seu projeto" />
+                        <textarea placeholder="Descrição do seu projeto" />
+                    </section>
+                    <section id="language">
+                        <h2>Personalização</h2>
+                        <div>
+                            <select>
+                                <option  value="Javascript">Javascript</option>
+                                <option  value="HTML">HTML</option>
+                                <option  value="CSS">CSS</option>
+                            </select>
+                            <input type="color" />
+                        </div>
+                    </section>
+                    <button>Salvar projeto</button>
+                </form>
             </main>
         </HomeWrapper>
     );
--- src/screen/Home/styles.ts
+++ src/screen/Home/styles.ts
@@ -6,38 +6,95 @@ export const HomeWrapper = styled.div`
 	height: 100vh;
 	font-family: "Inter", sans-serif;
 	main {
-		#projects {
+		form {
 			display: flex;
 			flex-direction: column;
-			gap: 16px;
-			h2 {
-				font-size: ${({ theme }) => theme.fonts.sm};
-				line-height: 18px;
-				letter-spacing: .4rem;
-				color: #FFF;
-				font-weight: 400;
-				text-transform: uppercase;
+			#projects {
+				display: flex;
+				flex-direction: column;
+				gap: 16px;
+				margin-top: 40px;
+				h2 {
+					font-size: ${({ theme }) => theme.fonts.sm};
+					line-height: 18px;
+					letter-spacing: .4rem;
+					color: #FFF;
+					font-weight: 400;
+					text-transform: uppercase;
+				}
+	
+				input {
+					outline: none;
+					border: none;
+					background-color: #FFFFFF29;
+					color: #FFF;
+					padding: 16px 14px;
+					border-radius: 8px;
+					flex: 1;
+				}
+				textarea {
+					outline: none;
+					border: none;
+					background-color: #FFFFFF29;
+					color: #FFF;
+					padding: 16px 0;
+					padding-left: 14px;
+					border-radius: 8px;
+					flex: 1;
+					font-family: inherit;
+				}
 			}
-
-			input {
-				outline: none;
-				border: none;
-				background-color: #FFFFFF29;
-				color: #FFF;
-				padding: 16px 14px;
-				border-radius: 8px;
+			#language {
+				display: flex;
+				flex-direction: column;
+				gap: 16px;
+				margin-top: 40px;
+				h2 {
+					font-size: ${({ theme }) => theme.fonts.sm};
+					line-height: 18px;
+					letter-spacing: .4rem;
+					color: #FFF;
+					font-weight: 400;
+					text-transform: uppercase;
+				}
+				div {
+					display: flex;
+					flex-direction: column;
+					gap: 16px;
+					select {
+						flex: 1;
+						padding: 16px 14px;
+						background-color: #FFFFFF29;
+						color: #FFF;
+						opacity: .64;
+						font-family: inherit;
+						border: none;
+						border-radius: 8px;
+						option {
+							color: #FFF;
+							background-color: ${({ theme}) => theme.colors.blue[800]};
+							opacity: .64;
+						}
+					}
+					input {
+						width: 100%;
+						height: 40px;
+						border: none;
+						border-radius: 4px;
+					}
+				}
+			}
+			button {
 				flex: 1;
-			}
-			textarea {
-				outline: none;
-				border: none;
-				background-color: #FFFFFF29;
-				color: #FFF;
+				margin-top: 40px;
 				padding: 16px 0;
-				padding-left: 14px;
+				border: none;
 				border-radius: 8px;
-				flex: 1;
-				font-family: inherit;
+				outline: none;
+				background-color: ${({ theme }) => theme.colors.blue[300]};
+				font-size: ${({ theme }) => theme.fonts.lg};
+				line-height: 24px;
+				color: #051D3B;
 			}
 		}
 	}

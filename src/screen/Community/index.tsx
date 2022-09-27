75bef226e481b96a417a1fc9a996aa2e072d5193
matheusgondra <matheusgondra951@gmail.com>
2022-09-27 18:23:59 -0300

    Criando página da comunidade

---
+++ src/screen/Community/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../../components/Header";
+import { MenuNav } from "../../components/MenuNav";
+
+export function Community() {
+	return (
+		<div>
+			<Header />
+			<main>
+				<MenuNav />
+			</main>
+		</div>
+	);
+}

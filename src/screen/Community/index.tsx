5c2ed3da94f412ee02ac2e167b07879bb84e03fb
matheusgondra <matheusgondra951@gmail.com>
2022-09-27 21:32:32 -0300

    Criando página comunidade mobile

--- src/screen/Community/index.tsx
+++ src/screen/Community/index.tsx
@@ -1,3 +1,6 @@
+import { ChatCircle, HeartStraight } from "phosphor-react";
+import { Author } from "../../components/Author";
+import { CodeEditor } from "../../components/CodeEditor";
 import { Header } from "../../components/Header";
 import { MenuNav } from "../../components/MenuNav";
 import { CommunityWrapper } from "./styles";
@@ -8,6 +11,37 @@ export function Community() {
 			<Header />
 			<main>
 				<MenuNav />
+				<section>
+					<div className="card">
+						<CodeEditor
+							colorEditor="purple" 
+							type="secondary"
+						/>
+						<div>
+							<div className="content">
+								<h2>Titulo do projeto</h2>
+								<p>Essa é a descrição do meu projeto</p>
+							</div>
+							<div className="actions">
+								<div className="icons">
+									<div>
+										<ChatCircle size={20} color="#ffffff" weight="fill" />
+										<span>9</span>
+									</div>
+									<div>
+										<HeartStraight size={20} color="#ffffff" weight="fill" />
+										<span>9</span>
+									</div>
+								</div>
+								<Author
+									name="@Matheus"
+									photo="https://github.com/matheusgondra.png"
+									visible
+								/>
+							</div>
+						</div>
+					</div>
+				</section>
 			</main>
 		</CommunityWrapper>
 	);
--- src/screen/Community/styles.ts
+++ src/screen/Community/styles.ts
@@ -1,5 +1,46 @@
 import styled from "styled-components";
 
 export const CommunityWrapper = styled.div`
-
+	padding: 0 16px;
+	padding-bottom: 40px;
+	height: 100%;
+	font-family: "Inter", sans-serif;
+	main {
+		margin-top: 40px;
+		section {
+			.card {
+				background-color: #00000029;
+				color: #FFF;
+				border-radius: 8px;
+				div {
+					.content {
+						padding: 24px;
+						h2 {
+							font-size: ${({ theme }) => theme.fonts.xl};
+							line-height: 32px;
+						}
+						p {
+							line-height: 24px;
+							opacity: .8;
+						}
+					}
+					.actions {
+						display: flex;
+						justify-content: space-between;
+						padding: 32px;
+						padding-top: 0;
+						.icons {
+							display: flex;
+							gap: 18px;
+							div {
+								display: flex;
+								align-items: center;
+								gap: 10px;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 `;

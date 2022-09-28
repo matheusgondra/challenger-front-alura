14aa3a1937a4eedd2d1a02a7de9a484c9fa88de2
matheusgondra <matheusgondra951@gmail.com>
2022-09-27 22:10:04 -0300

    Adicionando efeito de hover e focus no menu

--- src/components/MenuNav/index.tsx
+++ src/components/MenuNav/index.tsx
@@ -2,13 +2,17 @@ import { Code, UsersThree } from "phosphor-react";
 import { MenuWrapper } from "./styles";
 import { Link } from "react-router-dom";
 
-export function MenuNav() {
+interface Props {
+	page?: "home" | "community";
+}
+
+export function MenuNav({ page = "home" }: Props) {
     return (
         <MenuWrapper>
             <h2>Menu</h2>
             <nav>
                 <ul>
-                    <li>
+                    <li className={page === "community" ? "off" : undefined}>
 								<Link to="/">
 									<div>
 										<Code size={20} color="#FFF" weight="bold" />
@@ -16,7 +20,7 @@ export function MenuNav() {
 									<span>Editor de código</span>
 								</Link>
                     </li>
-                    <li className="off">
+                    <li className={page === "home" ? "off" : undefined}>
                         <Link to="/comunidade">
 									<div>
 										<UsersThree size={20} color="#FFF" weight="fill" />
--- src/components/MenuNav/styles.ts
+++ src/components/MenuNav/styles.ts
@@ -1,59 +1,60 @@
 import styled from "styled-components";
 
 export const MenuWrapper = styled.section`
-    display: none;    
-    @media screen and (min-width: 1024px){
-        display: block;
-        h2 {
-            font-size: ${({ theme }) => theme.fonts.sm};
-            color: #FFF;
-            letter-spacing: .4rem;
-            font-weight: 400;
-            text-transform: uppercase;
-            margin-bottom: 16px
-        }
-        nav {
-            ul {
-                list-style: none;
-                li {
-                    display: flex;
-                    width: 189px;
-                    text-align: center;
-                    margin-bottom: 16px;
-                    cursor: pointer;
-                    &:hover {
-                        div {
-                            background-color: ${({ theme }) => theme.colors.blue[200]};
-                        }
-                    }
-						  a {
+	display: none;
+	@media screen and (min-width: 1024px) {
+		display: block;
+		h2 {
+			font-size: ${({ theme }) => theme.fonts.sm};
+			color: #fff;
+			letter-spacing: 0.4rem;
+			font-weight: 400;
+			text-transform: uppercase;
+			margin-bottom: 16px;
+		}
+		nav {
+			ul {
+				list-style: none;
+				li {
+					display: flex;
+					width: 189px;
+					text-align: center;
+					margin-bottom: 16px;
+					cursor: pointer;
+					&:hover {
+						opacity: .72;
+					}
+					&:focus {
+						opacity: 1;
+					}
+					a {
+						display: flex;
+						align-items: center;
+						gap: 16px;
+						div {
+							width: 48px;
+							height: 48px;
+							border-radius: 16px;
+							background-color: ${({ theme }) => theme.colors.blue[300]};
 							display: flex;
-                    	align-items: center;
-							gap: 16px;
-							  div {
-									width: 48px;
-									height: 48px;
-									border-radius: 16px;
-									background-color: ${({ theme }) => theme.colors.blue[300]};
-									display: flex;
-									align-items: center;
-									justify-content: center;
-									
-									&:active {
-										 border: 4px solid rgba(80, 129, 251, 0.72);
-									}
-							  }
-	
-							  span {
-									color: #FFF;
-									flex: 1;
-							  }
-						  }
-                }
-                .off {
-                    opacity: 0.56;
-                }
-            }
-        }
-    }
+							align-items: center;
+							justify-content: center;
+
+							&:active {
+								border: 4px solid rgba(80, 129, 251, 0.72);
+							}
+						}
+
+						span {
+							color: #fff;
+							flex: 1;
+						}
+					}
+				}
+				.off {
+					opacity: 0.56;
+				}
+			}
+		}
+	}
 `;

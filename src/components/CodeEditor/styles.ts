ac6cab67418d2aabcbc1548d19e8a3084ea6c165
matheusgondra <matheusgondra951@gmail.com>
2022-10-02 10:31:07 -0300

    Importando tema "Tomorrow Night" do highlight.js

--- src/components/CodeEditor/styles.ts
+++ src/components/CodeEditor/styles.ts
@@ -11,7 +11,7 @@ export const CodeEditorWrapper = styled.div<Props>`
 	align-items: center;
 	flex-wrap: wrap;
 	width: 100%;
-	height: ${props => props.type === "primary" ? "492px" : "402px"};
+	height: ${(props) => (props.type === "primary" ? "492px" : "402px")};
 	border-radius: 8px;
 	background-color: ${(props) => props.colorEditor};
 	padding: 0 32px;
@@ -21,7 +21,7 @@ export const CodeEditorWrapper = styled.div<Props>`
 		width: 100%;
 		border-radius: 8px;
 		background-color: #141414;
-		height: ${props => props.type === "primary" ? "428px" : "354px"};
+		height: ${(props) => (props.type === "primary" ? "428px" : "354px")};
 		padding: 16px;
 		font-size: ${({ theme }) => theme.fonts.md};
 		img {
@@ -35,9 +35,82 @@ export const CodeEditorWrapper = styled.div<Props>`
 		}
 	}
 	@media screen and (min-width: 768px) {
-		height: ${props => props.type === "primary" ? "366px" : "342px"};
+		height: ${(props) => (props.type === "primary" ? "366px" : "342px")};
 		div {
-			height: ${props => props.type === "primary" ? "302px" : "294px"};
+			height: ${(props) => (props.type === "primary" ? "302px" : "294px")};
 		}
+	}
+`;
+
+export const Highlight = styled.pre`
+	/* Tomorrow Night Bright Theme */
+	/* Original theme - https://github.com/chriskempson/tomorrow-theme */
+	/* http://jmblog.github.com/color-themes-for-google-code-highlightjs */
+
+	/* Tomorrow Comment */
+	.hljs-comment,
+	.hljs-quote {
+		color: #969896;
+	}
+
+	/* Tomorrow Red */
+	.hljs-variable,
+	.hljs-template-variable,
+	.hljs-tag,
+	.hljs-name,
+	.hljs-selector-id,
+	.hljs-selector-class,
+	.hljs-regexp,
+	.hljs-deletion {
+		color: #d54e53;
+	}
+
+	/* Tomorrow Orange */
+	.hljs-number,
+	.hljs-built_in,
+	.hljs-literal,
+	.hljs-type,
+	.hljs-params,
+	.hljs-meta,
+	.hljs-link {
+		color: #e78c45;
+	}
+
+	/* Tomorrow Yellow */
+	.hljs-attribute {
+		color: #e7c547;
+	}
+
+	/* Tomorrow Green */
+	.hljs-string,
+	.hljs-symbol,
+	.hljs-bullet,
+	.hljs-addition {
+		color: #b9ca4a;
+	}
+
+	/* Tomorrow Blue */
+	.hljs-title,
+	.hljs-section {
+		color: #7aa6da;
+	}
+
+	/* Tomorrow Purple */
+	.hljs-keyword,
+	.hljs-selector-tag {
+		color: #c397d8;
+	}
+
+	.hljs {
+		background: black;
+		color: #eaeaea;
+	}
+
+	.hljs-emphasis {
+		font-style: italic;
+	}
+
+	.hljs-strong {
+		font-weight: bold;
 	}
 `;

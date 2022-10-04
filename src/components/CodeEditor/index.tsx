8406ff95cfa2792871199608a466e62ba0d6af61
matheusgondra <matheusgondra951@gmail.com>
2022-10-04 13:32:18 -0300

    bla

--- src/components/CodeEditor/index.tsx
+++ src/components/CodeEditor/index.tsx
@@ -8,11 +8,17 @@ interface Props {
 	type?: "primary" | "secondary";
 	language: string;
 	highlightTrigger: boolean;
+	setValue: (value: string | null) => void;
 }
 
-export function CodeEditor({ colorEditor, type = "primary", language = "javacript", highlightTrigger }: Props) {
+export function CodeEditor({ 
+	colorEditor,
+	type = "primary",
+	language,
+	highlightTrigger,
+	setValue
+}: Props) {
 	const code = useRef<HTMLElement>(null);
-
 	useEffect(() => {
 		document.querySelectorAll("pre code").forEach(el => {
 			hljs.highlightElement(el as HTMLElement);
@@ -26,7 +32,16 @@ export function CodeEditor({ colorEditor, type = "primary", language = "javacrip
 			}}>
 				<img src={macIcon} alt="botões do mac" />
 				<Highlight >
-					<code ref={code} className={`language-${language}`} contentEditable>const express = require("express");</code>
+					<code
+						ref={code}
+						className={`language-${language}`}
+						contentEditable
+						onInput={(e) => {
+							const target = e.target as HTMLElement;
+							setValue(target.textContent);
+						}}
+					>
+					</code>
 				</Highlight>
 			</div>
 		</CodeEditorWrapper>
--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -12,6 +12,7 @@ export function Home() {
 	const [highlight, setHighlight] = useState(false);
 	const [projectName, setProjectName] = useState("");
 	const [description, setDescription] = useState("");
+	const [codeContent, setCodeContent] = useState<string | null>("");
 
 	return (
 		<HomeWrapper colorEditor={colorEditor}>
@@ -19,10 +20,23 @@ export function Home() {
 			<main>
 				<MenuNav />
 				<section id="code-editor">
-					<CodeEditor highlightTrigger={highlight} language={language} colorEditor={colorEditor} />
+					<CodeEditor 
+						highlightTrigger={highlight}
+						language={language}
+						colorEditor={colorEditor}
+						setValue={setCodeContent}
+					/>
 					<button onClick={() => setHighlight(!highlight)}>Visualizar com o highlight</button>
 				</section>
-				<form>
+				<form onSubmit={(e) => {
+					e.preventDefault();
+					console.log("Nome do projeto: ", projectName);
+					console.log("Descrição do projeto: ", description);
+					console.log("Cor do projeto: ", colorEditor);
+					console.log("Codigo do projeto: ", codeContent);
+					console.log("Linguagem do projeto: ", language);
+
+				}}>
 					<section id="projects">
 						<h2>Seu projeto</h2>
 						<input
@@ -40,6 +54,7 @@ export function Home() {
 						<h2>Personalização</h2>
 						<div className="language-container">
 							<select onChange={e => setLanguage(e.target.value)}>
+								<option>Escolha uma linguagem</option>
 								<option value="Javascript">Javascript</option>
 								<option value="HTML">HTML</option>
 								<option value="CSS">CSS</option>
